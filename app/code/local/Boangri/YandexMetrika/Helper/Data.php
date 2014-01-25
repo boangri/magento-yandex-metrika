aa3eb312b1092b75ed13689a5b8ce782e0e5bd95
Boris Gribovskiy <xinu@yandex.ru>
2014-01-25 23:24:07 +0400

    initial commit version 0.0.1

---
+++ app/code/local/Boangri/YandexMetrika/Helper/Data.php
@@ -0,0 +1,5 @@
+<?php
+class Boangri_YandexMetrika_Helper_Data extends Mage_Core_Helper_Abstract
+{
+    
+}    
---
+++ app/code/local/Boangri/YandexMetrika/controllers/YandexmetrikaController.php
@@ -0,0 +1,9 @@
+<?php
+class Boangri_YandexMetrika_YandexmetrikaController extends Mage_Adminhtml_Controller_Action
+{
+    public function indexAction()
+    {
+        $this->loadLayout();
+        $this->renderLayout();
+    }
+}
